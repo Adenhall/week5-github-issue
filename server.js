af013f5c25b8ff6723551aeb1a5ad89c6f260000
yogitravel <64913300+thuongbien@users.noreply.github.com>
2020-06-05 14:22:00 +0700

    thuong part

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-const http = require('http');
-const request = require('request');
-require('dotenv').config();
+const http = require("http");
+const request = require("request");
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
-const secretKey = process.env.REACT_APP_SECRET_KEY; 
+const secretKey = process.env.REACT_APP_SECRET_KEY;
 
-console.log('started server on port 5000');
+console.log("started server on port 5000");
 
-http.createServer((req, res) => {
-  var code = req.url.split("=")[1];
-  if (code) {
-    request.post('https://github.com/login/oauth/access_token', {
-      form: {
-        client_id: clientId,
-        client_secret: secretKey,
-        code: code
-      }
-    }, (err, r, body) => {
-      res.writeHead(301, {
-        'Location': 'http://localhost:3000?' + body // client URL
-      });
-      res.end();
-    })
-    
-  } else {
-    res.writeHead(404);
-    res.end();
-  }
-}).listen(5000); // server port number
+http
+	.createServer((req, res) => {
+		var code = req.url.split("=")[1];
+		if (code) {
+			request.post(
+				"https://github.com/login/oauth/access_token",
+				{
+					form: {
+						client_id: clientId,
+						client_secret: secretKey,
+						code: code,
+					},
+				},
+				(err, r, body) => {
+					res.writeHead(301, {
+						Location: "http://localhost:3000?" + body, // client URL
+					});
+					res.end();
+				}
+			);
+		} else {
+			res.writeHead(404);
+			res.end();
+		}
+	})
+	.listen(5000); // server port number
--- src/App.js
+++ src/App.js
@@ -1,77 +1,84 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import { Button, Container, Row, Column } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Button, Container, Row, Column } from "react-bootstrap";
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
+let inputString = "";
 
 function App() {
-  const [token,setToken] = useState(null)
+	const [token, setToken] = useState(null);
+	const [respoSearch, setRespoSearch] = useState(null);
+	const [userSearch, setUserSearch] = useState(null);
+	const [dataList, setDataList] = useState([]);
+	const getToken = () => {
+		const existingToken = localStorage.getItem("token"); // if we already have token in our localstorage, just get that
+		const accessToken = window.location.search.split("=")[0] === "?access_token" ? window.location.search.split("=")[1].split("&")[0] : null; // reads the token value from body (url)
 
-  const getToken = () => {
-    const existingToken = localStorage.getItem('token'); // if we already have token in our localstorage, just get that
-    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1].split("&")[0] : null; // reads the token value from body (url)
+		if (!accessToken && !existingToken) {
+			window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`);
+		}
 
-    if (!accessToken && !existingToken) {
-      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
-    }
+		if (accessToken) {
+			// A.1 if you get token value from url
+			console.log(`New accessToken: ${accessToken}`);
 
-    if (accessToken) { // A.1 if you get token value from url
-      console.log(`New accessToken: ${accessToken}`);
+			localStorage.setItem("token", accessToken); // A.2 save it to localStorage
+			setToken(accessToken); // A.3 and set the state
+		}
 
-      localStorage.setItem("token", accessToken); // A.2 save it to localStorage
-      setToken(accessToken) // A.3 and set the state
-    }
+		if (existingToken) {
+			// A.4 if you have token on your localStorage already
+			setToken(existingToken);
+		}
+	};
 
-    if (existingToken) { // A.4 if you have token on your localStorage already
-      setToken(existingToken)
-    }
-  }
+	const postNewIssue = async () => {
+		// const issue = { title: title, body: details }; // made this as object type to change to json
+		const issue = { title: "title", body: "details" };
+		const url = `https://api.github.com/repos/Yogitravel/GithubIssues/issues`;
+		const response = await fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				Authorization: `token ${token}`,
+			},
+			body: JSON.stringify(issue),
+		});
+		console.log("response?", response);
+	}; // how to make a new issue
 
-  const postNewIssue = async() => {
-    // const issue = { title: title, body: details }; // made this as object type to change to json
-    const issue = { title: "title", body: "details" }; 
-    const url = `https://api.github.com/repos/christinapbui/GithubIssues/issues`;
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `token ${token}`
-      },
-      body: JSON.stringify(issue)
-    });
-    console.log("response?",response)
-  } // how to make a new issue
+	useEffect(() => {
+		getToken();
+		postNewIssue();
+	}, []);
 
-  useEffect(()=>{
-    getToken()
-  },[])
-      
-  const getIssues = async() => {
-    let url = `https://api.github.com/repos/facebook/react/issues`
-    let data = await fetch(url)
-    let result = await data.json()
-    console.log("result?",result)
-  } // this is how to get the data
+	const getIssues = async () => {
+		let tempArray = inputString.split("/");
+		let url = `http://api.github.com/repos/${tempArray[0]}/${tempArray[1]}/issues`;
+		let data = await fetch(url);
+		let result = await data.json();
+		setDataList(result);
+		console.log("Data nhan duoc cua get ", dataList);
+	}; // this is how to get the data
 
+	if (dataList !== null)
+		return (
+			<div>
+				<Container className="navbar">
+					<input onChange={(e) => (inputString = e.target.value)} placeholder="RespoSearch here..."></input>
+					<Button onClick={() => getIssues()}>search</Button>
 
-
-  return (
-    <div>
-      <Container className="navbar">
-        <input value="text" placeholder="Search here..."></input><Button onClick={()=>getIssues()}>search</Button>
-      </Container>
-      <Container className="issues-area">
-      <Button onClick={()=>postNewIssue()}>Post new issue</Button>
-      <h1>Issue Title</h1>
-      <h4>Owner of Issue</h4>
-      <h4>Owner Avatar</h4>
-      <h4>When Issue was Created</h4>
-      <h4>Body of Issue</h4>
-      <h4>Label</h4>
-      <h4>State of Issue</h4>
-      </Container>
-    </div>
-  );
+					<ul>
+						{dataList.map((item) => {
+							return <li>{item.body}</li>;
+						})}
+					</ul>
+				</Container>
+				<Container className="issues-area">
+					<Button onClick={() => postNewIssue()}>Post new issue</Button>
+				</Container>
+			</div>
+		);
 }
 
 export default App;
