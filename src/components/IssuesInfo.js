0861c224e08ded39f1430bd5c02099945f6aac61
ivy <ivyvodesign@gmail.com>
2020-06-07 19:03:17 +0700

    merging

--- src/components/IssuesInfo.js
+++ src/components/IssuesInfo.js
@@ -4,6 +4,8 @@ import moment from 'moment'
 import TitleModal from './TitleModal.js'
 import Button from "react-bootstrap/Button";
 import ReactMarkdown from 'react-markdown';
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 
 
 export default function IssuesInfo(props) {
@@ -28,6 +30,7 @@ export default function IssuesInfo(props) {
                     {props.issuesListProps.map(item => {
                         return (
                             <>
+            
                             <div className="issueslist">
                                 <div style={{marginLeft: "10px"}}>
                                     <div onClick={() => openTitleModal()}><h4><a className="issue-title" href="#"><i class="fas fa-exclamation-circle" style={{margin: "10px 10px 10px 0px"}}></i>Issue: {item.title}</a></h4></div>
@@ -36,9 +39,22 @@ export default function IssuesInfo(props) {
                                     <div></div> {moment(item.created_at).startOf('day').fromNow()} by {item.user.login}</p></div>
                                 </div>
                                 <div style={{margin: "10px 10px"}} className="d-flex flex-column align-items-end"><img src={item.user.avatar_url} width="40" height="40" /><div><b>State: {item.state.toUpperCase()}</b></div></div>
-                                
+                               
                             </div>
-                            <ReactMarkdown source={item.body} />
+                            <div className="issueslist">
+                                <Accordion>
+                                    <Card.Text>
+                                    <Accordion.Toggle as={Button} variant="link" eventKey="0" style={{ paddingLeft: 0 }}>
+                                        Read More
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse className="markdown" eventKey="0">
+                                        <ReactMarkdown source={item.body} />
+                                    </Accordion.Collapse>
+                                    </Card.Text>
+                                </Accordion>
+                            </div>
+                            
+                        
                             <TitleModal item={item} titleModalProps={titleModal} closeTitleModalProps={closeTitalModal}/>
                             </>
                     )
